8ab99f25365e2890c89930a7f2857ee9c82aca24
wenjietehan <148920295+wenjietehan@users.noreply.github.com>
2024-12-17 13:22:50 -0800

    Create Web_Summary_ollama.py

---
+++ Web_Summary_ollama.py
@@ -0,0 +1,69 @@
+# imports
+
+import os
+import requests
+import ollama
+from bs4 import BeautifulSoup
+from IPython.display import Markdown, display
+
+# A class to represent a Webpage
+# If you're not familiar with Classes, check out the "Intermediate Python" notebook
+
+# Some websites need you to use proper headers when fetching them:
+headers = {
+ "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36"
+}
+
+class Website:
+    """
+    A utility class to represent a Website that we have scraped
+    """
+
+    url: str
+    title: str
+    text: str
+    
+    def __init__(self, url):
+        """
+        Create this Website object from the given url using the BeautifulSoup library
+        """
+        self.url = url
+        response = requests.get(url, headers=headers)
+        soup = BeautifulSoup(response.content, 'html.parser')
+        self.title = soup.title.string if soup.title else "No title found"
+        for irrelevant in soup.body(["script", "style", "img", "input"]):
+            irrelevant.decompose()
+        self.text = soup.body.get_text(separator="\n", strip=True)
+
+system_prompt = "You are an assistant that analyzes the contents of a website \
+and provides a short summary, ignoring text that might be navigation related. \
+Respond in markdown."
+
+def user_prompt_for(website):
+    user_prompt = f"You are looking at a website titled {website.title}"
+    user_prompt += "\nThe contents of this website is as follows; \
+please provide a short summary of this website in markdown. \
+If it includes news or announcements, then summarize these too.\n\n"
+    user_prompt += website.text
+    return user_prompt
+
+def messages_for(website):
+    return [
+        {"role": "system", "content": system_prompt},
+        {"role": "user", "content": user_prompt_for(website)}
+    ]
+
+def summarize(url):
+    website = Website(url)
+    
+    response = ollama.chat(
+        model=MODEL,
+        messages = messages_for(website)
+    )
+    return response['message']['content']
+
+def display_summary(url):
+    summary = summarize(url)
+    display(Markdown(summary))
+
+display_summary("https://edwarddonner.com")
